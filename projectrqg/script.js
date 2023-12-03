be1b6458616c86ea19da528def69194f0a634ecb
Mubby03 <135833377+Mubby03@users.noreply.github.com>
2023-12-03 10:51:47 +0100

    Add files via upload

--- projectrqg/script.js
+++ projectrqg/script.js
@@ -1,3 +1,6 @@
+const containerColors = ['#FFDAB9', '#ADD8E6', '#DDA0DD', '#FFE4B5', '#AFEEEE', '#E6E6FA'];
+let currentColorIndex = 0;
+
 document.addEventListener("DOMContentLoaded", function () {
     getRandomQuote();
 });
@@ -7,6 +10,7 @@ function getRandomQuote() {
         .then(response => response.json())
         .then(data => {
             displayQuote(data);
+            updateContainerColor();
         })
         .catch(error => console.error("Error fetching quotes:", error));
 }
@@ -14,7 +18,9 @@ function getRandomQuote() {
 function displayQuote(quotes) {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const quoteText = quotes[randomIndex].text;
-    const quoteAuthor = quotes[randomIndex].author || "Unknown";
+    let quoteAuthor = quotes[randomIndex].author || "Unknown";
+
+    quoteAuthor = quoteAuthor.replace(/, type\.fit$/, '');
 
     const quoteElement = document.getElementById("quote");
     const authorElement = document.getElementById("author");
@@ -25,3 +31,33 @@ function displayQuote(quotes) {
     
     tweetBtn.href = `https://twitter.com/intent/tweet?text="${quoteText}" - ${quoteAuthor}`;
 }
+
+function updateContainerColor() {
+    const container = document.querySelector('#quote');
+    const container_a = document.querySelector('#author')
+
+    const buttonC = document.querySelector('#new-quote-btn')
+    const buttonD = document.querySelector('#tweet-btn')
+    const buttonE = document.querySelector('#btnsz')
+    
+    container.style.color = containerColors[currentColorIndex];
+    container_a.style.color = containerColors[currentColorIndex];
+    buttonC.style.backgroundColor = containerColors[currentColorIndex];
+    buttonD.style.backgroundColor = containerColors[currentColorIndex];
+    buttonE.style.backgroundColor = containerColors[currentColorIndex];
+    currentColorIndex = (currentColorIndex + 1) % containerColors.length;
+}
+function copyQuote() {
+    const quoteText = document.getElementById("quote").innerText;
+    const authorText = document.getElementById("author").innerText;
+    const fullQuote = `"${quoteText}" - ${authorText}`;
+
+    const textarea = document.createElement("textarea");
+    textarea.value = fullQuote;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+
+    alert("Quote copied to clipboard!");
+  }
